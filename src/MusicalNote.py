a75cc1126d3c2faa54b1fd924d210eb2748de6ad
Alberto de Santos <a.desantos.sierra@gmail.com>
2018-09-24 23:14:14 +0200

    #5 Adding a constructor more robust to all the different type of alterations. It is able also to create a Note even if not all the paramaters are present

--- src/MusicalNote.py
+++ src/MusicalNote.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 # Create Musical Notes (Do, Re, Mi, ...)
 class MusicalNote(object):
 
@@ -9,16 +11,42 @@ class MusicalNote(object):
 	# Intensity (louder/soft)
 	# Timbre (instrument you use to play)
 
+	
+
 	def __init__(self, **notes_props):
+
+		# Only pitch is present
+		# if (len(notes_props) == 1) & (notes_props.keys() == ['pitch']):
+		# 	self.pitch = notes_props['pitch']
+		# else:
+		# 	self.notes_props = notes_props
+		# 	self.duration = notes_props['duration']
+		# 	self.pitch = notes_props['pitch']
+		# 	self.intensity = notes_props['intensity']
+		# 	self.timbre = notes_props['timbre']
+
+		self.alteration = ''
+
+		# As it is used by Melody class
 		self.notes_props = notes_props
-		self.duration = notes_props['duration']
-		self.pitch = notes_props['pitch']
-		self.intensity = notes_props['intensity']
-		self.timbre = notes_props['timbre']
+		
+		if 'pitch' in notes_props:
+			self.pitch = notes_props['pitch']
+
+		if 'duration' in notes_props:
+			self.duration = notes_props['duration']
+
+		if 'intensity' in notes_props:
+			self.intensity = notes_props['intensity']
+
+		if 'timbre' in notes_props:
+			self.timbre = notes_props['timbre']
 
 		# Check whether there is alteration on the properties of the notes
 		if 'alteration' in notes_props:
 			self.alteration = notes_props['alteration']
+			self.pitch = self.pitch + int(dic_alterations.keys()[dic_alterations.values().index(self.alteration)])	
+			self.notes_props['pitch'] = self.pitch
 
 	def get_pitch(self):
 		return self.pitch
@@ -28,6 +56,25 @@ class MusicalNote(object):
 
 	def get_props(self):
 		return self.notes_props
+
+	
+		
+
+
+	def to_string(self):
+		# Combine the name of the note (Do, Re, ...) with their corresponding alteration if any
+		return self.__class__.__name__+self.alteration
+
+	def get_major_chord(self):
+		tonic_note = globals()[self.__class__.__name__]()
+		return [tonic_note, tonic_note.get_note_from_interval('3M'), tonic_note.get_note_from_interval('5P')]
+
+	def get_minor_chord(self):
+		tonic_note = globals()[self.__class__.__name__]()
+		return [tonic_note, tonic_note.get_note_from_interval('3m'), tonic_note.get_note_from_interval('5P')]
+
+	def get_dis_chord(self):
+		return self.pitch + np.cumsum([0, half_tone+tone, half_tone+tone])
 
 
 class Do(MusicalNote):
@@ -93,3 +140,13 @@ class Si(MusicalNote):
 		# Sharp (#) and flat (b)
 		notes_props.update({'pitch':71})
 		super(self.__class__, self).__init__(**notes_props)
+
+
+if __name__ == '__main__':
+	do_props = {'duration':100, 'intensity':50, 'timbre':1}
+	print(Si(**{'alteration':'b'}).get_note_from_interval('5P').get_pitch())
+	print('test')
+	print(Do().get_note_from_interval('3m'))
+	print(Do().get_note_from_interval('3m').get_pitch())
+	print(Do().get_major_chord()[2].get_pitch())
+	
