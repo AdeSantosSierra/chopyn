cc7fffba15fd9ee41591cbb66de88ccbcba9254f
Alberto de Santos <a.desantos.sierra@gmail.com>
2018-09-24 23:19:26 +0200

    Rearrange melody.py class so that it can create both Random music and music from a sequence of notes

--- src/melody.py
+++ src/melody.py
@@ -10,7 +10,35 @@ import pandas as pd
 # Random Melody
 # A Melody is a sequence of notes
 
-class RandomMelody(object):
+
+class Melody (object):
+
+
+
+	def convert_to_midi(self):
+
+		start_ms_array = \
+		(np.cumsum([0]+[note_from_melody.get_duration() 
+			for note_from_melody in self.sequence_of_notes]))
+
+		# Create dataframe with musical properties from sequence/melody
+		# print([note_from_melody.get_play_props() for note_from_melody in self.sequence_of_notes])
+		# print('pppppppp')
+		melody_dataframe = pd.DataFrame.from_records([note_from_melody.get_play_props() 
+			for note_from_melody in self.sequence_of_notes])
+
+		# print(melody_dataframe)
+
+		# Add the time sequence
+		melody_dataframe['start_ms'] = start_ms_array[:-1]
+
+		# Rename columns as the MIDI has already specific names for the columns
+		# print(melody_dataframe.columns)
+		melody_dataframe.columns = ['dur_ms','velocity','pitch','part','start_ms']
+
+		return melody_dataframe	
+
+class RandomMelody(Melody):
 
 	def __init__(self):
 
@@ -27,28 +55,10 @@ class RandomMelody(object):
 												   'timbre':1}) 
 							      for iterator_note in random_sequence]
 
-		
 
-	def convert_to_midi(self):
-
-		start_ms_array = \
-		(np.cumsum([0]+[note_from_melody.get_duration() 
-			for note_from_melody in self.sequence_of_notes]))
-
-		# Create dataframe with musical properties from sequence/melody
-		melody_dataframe = pd.DataFrame.from_records([note_from_melody.get_props() 
-			for note_from_melody in self.sequence_of_notes])
-
-		# Add the time sequence
-		melody_dataframe['start_ms'] = start_ms_array[:-1]
-
-		# Rename columns as the MIDI has already specific names for the columns
-		melody_dataframe.columns = ['dur_ms','velocity','pitch','part','start_ms']
-
-		return melody_dataframe	
-
-
-
+class SequenceMelody(Melody):
+	def __init__(self,sequence_of_notes):
+		self.sequence_of_notes = sequence_of_notes
 
 
 if __name__ == '__main__':
