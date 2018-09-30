b4705b1fe970dfbb89c76251f5aa22ffa5f0047b
Alberto de Santos <a.desantos.sierra@gmail.com>
2018-09-30 02:21:30 +0200

    #12 get the most common note as the most played note, not only in terms of time but also in terms of time duration

--- src/ReadMusic.py
+++ src/ReadMusic.py
@@ -7,6 +7,7 @@ from Play import CSVtoMIDI
 from Tonality import *
 
 import numpy as np
+import pandas as pd
 
 
 class Score(object):
@@ -18,14 +19,53 @@ class Read(Score):
 
 	def __init__(self, name_file_midi):
 
-		
-		midi_file = open(name_file_midi, 'r')
+		# Read midi file
+		self.music_df = pd.read_csv(name_file_midi)
 
-		print(midi_file)
+	def get_music_data(self):
+		return self.music_df
 
+	
+
+
+	def get_most_common_note(self):
+
+		# Names can be F3#, A2#, then return F# and A#
+		# Names can be F3, A2, then return F and A
+		self.music_df['name_note'] = \
+		(self.music_df
+		 .apply(lambda x:
+		        _get_note_name_without_octave(x['fullNoteOctave']), axis=1)
+		 )
+
+
+		return (self
+		      .music_df
+		      # Group by name of the note (A, F#, ...)
+		      .groupby(['name_note'])
+		      # Take the total length that note has been played
+		      .agg({'dur_ms':sum})
+		      .rename(columns={'dur_ms': 'cum_duration'})
+		      # Order in descending manner
+		      .sort_values(['cum_duration'],ascending=False)
+		      .reset_index()
+		      # Take the first element and the corresponding column
+		      .head(1)['name_note'][0]
+		      )
+
+def _get_note_name_without_octave(fullNoteOctave):
+	# Function to get the name, regardless the octave
+	if len(fullNoteOctave) == 3:
+		# Names can be F3#, A2#, then return F# and A#
+		return fullNoteOctave[::2]
+	else:
+		# Names can be F3, A2, then return F and A
+		return fullNoteOctave[0]
 
 if __name__ == "__main__":
-	name_file_midi = '../../scores/Chopn_Etude_Op_10_n_1.csv'
-	Read(name_file_midi)
+	name_file_midi = '../../scores/Debussy_Claire_de_Lune.csv'
+	chopin = Read(name_file_midi)
+	# print(chopin.get_music_data().head())
+	print(chopin.get_most_common_note())
 
 
